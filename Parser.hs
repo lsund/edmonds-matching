00ec8e2e605fc6d86eca529ee1148bbc1df4048b
Ludvig Sundström <lud.sund@gmail.com>
2017-04-27 17:46:39 +0200

    adding support for comments

--- Parser.hs
+++ Parser.hs
@@ -2,14 +2,16 @@
 module Parser where
 
 import Data.Char
+import Data.List
 
-data DimacsEntry = Dimension Int Int | Edge Int Int
+data DimacsEntry = Dimension Int Int | Edge Int Int | Comment String
     deriving (Show)
 
 parseLine :: [String] -> DimacsEntry
 parseLine ["p", "edge", x, y] = Dimension (read x) (read y)
 parseLine ["e", x, y]         = Edge (read x) (read y)
 parseLine ["a", x, y]         = Edge (read x) (read y)
+parseLine ("c" : xs)          = Comment $ unwords xs
 
 linesToEdges :: [String] -> [DimacsEntry]
 linesToEdges = map (parseLine . words)
