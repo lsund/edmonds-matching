8da003c6424fed2c9a866a06d3f69deceef682d0
Ludvig Sundström <lud.sund@gmail.com>
2017-05-01 19:19:06 +0200

    refactoring and renaming

--- Matching.hs
+++ Matching.hs
@@ -3,14 +3,17 @@ module Matching (edmonds) where
 
 import Prelude hiding (lookup)
 import Data.Graph
+import Data.Array
 import Data.Maybe
+import Data.List hiding (lookup)
 import qualified Data.Map as Map
 
 type Map   = Map.Map
-data State = State { mu      :: Map Int Int
-                   , phi     :: Map Int Int
-                   , ro      :: Map Int Int
-                   , scanned :: Map Int Int }
+type VertexAssoc = Map Vertex Vertex
+data State = State { mu      :: VertexAssoc
+                   , phi     :: VertexAssoc
+                   , ro      :: VertexAssoc
+                   , scanned :: Map Vertex Bool }
 
 applyEveryOther :: (a -> a) -> (a -> a) -> a -> [a]
 applyEveryOther = applyEveryOther' True
@@ -18,37 +21,46 @@ applyEveryOther = applyEveryOther' True
         applyEveryOther' True f g x = x : applyEveryOther' False f g (f x) 
         applyEveryOther' False f g x = x :  applyEveryOther' True f g (g x)
 
-lookup :: Map Int Int -> (Int -> Int)
+lookup :: VertexAssoc -> (Vertex -> Vertex)
 lookup m x = fromJust $ Map.lookup x m 
 
-p :: State -> Int -> [Int]
+p :: State -> Vertex -> [Vertex]
 p (State f g _ _) = applyEveryOther (lookup f) (lookup g)
 
-isOuter :: State -> Int -> Bool
+isOuter :: State -> Vertex -> Bool
 isOuter (State f g _ _) v = fv == v || lookup g fv /= fv
     where fv = lookup f v 
 
-isInner :: State -> Int -> Bool
+isInner :: State -> Vertex -> Bool
 isInner (State f g _ _) v = lookup g fv == fv && gv /= v
     where 
         fv  = lookup f v
         gv = lookup g v
 
-isOutOfForest :: State -> Int -> Bool
+isOutOfForest :: State -> Vertex -> Bool
 isOutOfForest (State f g _ _) v = fv /= v && gv == v && gfv == fv
     where
         fv  = lookup f v
         gv = lookup g v
         gfv = lookup g fv
-        
-edmonds graph =
+
+neighbours :: Graph -> Vertex -> [Vertex]
+neighbours graph v = graph ! v
+
+initializeState :: Graph -> State
+initializeState graph = 
     let nv    = (length . vertices) graph
         ne    = (length . edges) graph
         idMap = Map.fromList [(x, x) | x <- [1..nv]]
-        fInit = idMap
-        gInit = idMap
-        hInit = idMap
-        sInit = Map.fromList [(x, y) | x <- [1..nv], y <- replicate 10 False]
-    in
-        fInit
+        sInit = Map.fromList [(x, y) | x <- [1..nv], y <- replicate nv False]
+    in State idMap idMap idMap sInit
+
+edmonds graph =
+    let state = initializeState graph
+        -- Map.assocs has RT O(n)
+    in case find (\(_, y) -> not y) (Map.assocs (scanned state)) of 
+            Nothing -> print "No unscanned outer edges. Terminating"
+            Just (x, _) -> 
+                let nbs = neighbours graph x
+                in print nbs
 
