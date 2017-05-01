9596b61f62f3324766a455a92dbfbb67b1c0dc83
Ludvig Sundström <lud.sund@gmail.com>
2017-05-01 18:47:49 +0200

    changing state to a datatype

--- Matching.hs
+++ Matching.hs
@@ -7,7 +7,10 @@ import Data.Maybe
 import qualified Data.Map as Map
 
 type Map   = Map.Map
-type State = (Map Int Int, Map Int Int, Map Int Int)
+data State = State { mu      :: Map Int Int
+                   , phi     :: Map Int Int
+                   , ro      :: Map Int Int
+                   , scanned :: Map Int Int }
 
 applyEveryOther :: (a -> a) -> (a -> a) -> a -> [a]
 applyEveryOther = applyEveryOther' True
@@ -19,36 +22,33 @@ lookup :: Map Int Int -> (Int -> Int)
 lookup m x = fromJust $ Map.lookup x m 
 
 p :: State -> Int -> [Int]
-p (f, g, _) = applyEveryOther (lookup f) (lookup g)
+p (State f g _ _) = applyEveryOther (lookup f) (lookup g)
 
 isOuter :: State -> Int -> Bool
-isOuter (f, g, _) v = fv == v || lookup g fv /= fv
+isOuter (State f g _ _) v = fv == v || lookup g fv /= fv
     where fv = lookup f v 
 
 isInner :: State -> Int -> Bool
-isInner (f, g, _) v = lookup g fv == fv && gv /= v
+isInner (State f g _ _) v = lookup g fv == fv && gv /= v
     where 
         fv  = lookup f v
         gv = lookup g v
 
 isOutOfForest :: State -> Int -> Bool
-isOutOfForest (f, g, _) v = fv /= v && gv == v && gfv == fv
+isOutOfForest (State f g _ _) v = fv /= v && gv == v && gfv == fv
     where
         fv  = lookup f v
         gv = lookup g v
         gfv = lookup g fv
         
-scanned = undefined
-
 edmonds graph =
-    let nVertices   = (length . vertices) graph
-        nEdges      = (length . edges) graph
-        idMap       = Map.fromList [(x, x) | x <- [1..nVertices]]
-        fInit       = idMap
-        gInit     = idMap
-        roInit      = idMap
-        scannedInit = Map.fromList 
-                        [(x, y) | x <- [1..nVertices], y <- replicate 10 False]
+    let nv    = (length . vertices) graph
+        ne    = (length . edges) graph
+        idMap = Map.fromList [(x, x) | x <- [1..nv]]
+        fInit = idMap
+        gInit = idMap
+        hInit = idMap
+        sInit = Map.fromList [(x, y) | x <- [1..nv], y <- replicate 10 False]
     in
         fInit
 
