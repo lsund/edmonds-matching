a7599fdafd5cd3c5ced64688c61c9d6a27f62631
Ludvig Sundström <lud.sund@gmail.com>
2017-05-01 08:43:40 +0200

    chaniging tree implementation to Data.Set
    now have logn operations. Before, we needed O(n) to search the tree

--- Main.hs
+++ Main.hs
@@ -3,9 +3,11 @@ module Main where
 
 import Matching
 import DIMACSParser
+import Tree
 
 main :: IO ()
 main = do
     let file = "data/K4.dmx"
     graph <- fileToGraph file
-    print $ edmonds graph
+    print graph
+    -- print $ edmonds graph
--- Tree.hs
+++ Tree.hs
@@ -1,35 +1,44 @@
 module Tree where
 
-import Data.Tree
+import qualified Data.Map as Map
+
+type Tree = (Int, Map.Map Int [Int])
 
 ----------------------------------------------------------------------------
 -- dummy values
 
-dummy = insert 5 2 $ insert 4 2 $ insert 2 1 $ insert 3 1 $ singleton 1
+dummy :: Tree
+dummy = insert 4 2 $
+        insert 5 2 $
+        insert 3 1 $
+        insert 2 1 $
+        insert 6 5 $
+        singleton 1
 
 ----------------------------------------------------------------------------
 -- Construction
 
-singleton :: Int -> Tree Int
-singleton x = Node x []
+singleton :: Int -> Tree
+singleton x = (x, Map.singleton x [])
 
-insert :: Int -> Int -> Tree Int -> Tree Int
-insert x under (Node v cs)
-    | under == v = Node v (Node x [] : cs)
-    | otherwise  = Node v (map (insert x under) cs)
+insert :: Int -> Int -> Tree -> Tree
+insert x at (root, tree) =
+    case Map.lookup at tree of
+        Just nbs -> (root, Map.adjust (x :) at tree)
+        Nothing  -> (root, Map.insert at [x] tree)
 
 ----------------------------------------------------------------------------
 -- Properties
 
 -- All vertices with even distances to the root (The root itself is outer)
-outer :: Tree Int -> [Int]
-outer = outer' True
-    where
-        outer' True (Node v cs) = v : concatMap (outer' False) cs
-        outer' False (Node v cs) = concatMap (outer' True) cs
+outer :: Tree -> [Int]
+outer (root, tree) = root : outer' False tree root
 
-----------------------------------------------------------------------------
--- Utilities
+outer' :: Bool -> Map.Map Int [Int] -> Int -> [Int]
+outer' flag tree at =
+    case Map.lookup at tree of
+        Just nbs -> 
+            let new = if flag then nbs else []
+            in  new ++ concatMap (outer' (not flag) tree) nbs
+        Nothing -> []
 
-toScreen :: Tree Int -> IO ()
-toScreen = putStrLn . drawTree . fmap show
