dc75d381fe26c8ef5a09deec156f5963d64a0aa4
Ludvig Sundström <lud.sund@gmail.com>
2017-05-08 18:51:19 +0200

    changing String to Text

--- src/DIMACSParser.hs
+++ src/DIMACSParser.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fwarn-unused-imports #-}
 
 module DIMACSParser (fileToGraph) where
 
-import Prelude
-import Data.Char
+import Protolude
 import Data.Graph
+import qualified Data.Text as Text
+import qualified Data.Text.Read as Text
 
 data DimacsEntry = DimacsSize { nvertices :: Int
                               , nedges    :: Int
@@ -11,25 +14,29 @@ data DimacsEntry = DimacsSize { nvertices :: Int
                  | DimacsEdge { first     :: Int
                               , second    :: Int
                               }
-                 | DimacsComment { content   :: String } 
-                 deriving (Show, Eq)
+                 | DimacsComment { content   :: Text } deriving (Show, Eq)
 
-parseLine :: [String] -> DimacsEntry
-parseLine ["p", "edge", x, y] = DimacsSize (read x) (read y)
-parseLine ["e", x, y]         = DimacsEdge (read x) (read y)
-parseLine ["a", x, y]         = DimacsEdge (read x) (read y)
-parseLine ("c" : xs)          = DimacsComment $ unwords xs
+stringToInt :: Text -> Int
+stringToInt x = case Text.decimal x of
+                    Left a -> undefined
+                    Right b -> fst b
 
-loadLines :: [String] -> [DimacsEntry]
-loadLines = map (parseLine . words)
+parseLine :: [Text] -> DimacsEntry
+parseLine ["p", "edge", x, y] = DimacsSize (stringToInt x) (stringToInt y)
+parseLine [h, x, y]
+    | h == "e" || h == "a"    = DimacsEdge (stringToInt x) (stringToInt y)
+parseLine ("c" : xs)          = DimacsComment $ Text.unwords xs
+
+loadLines :: [Text] -> [DimacsEntry]
+loadLines = map (parseLine . Text.words)
 
 loadFile :: FilePath -> IO [DimacsEntry]
-loadFile path = fmap (loadLines . lines) (readFile path)
+loadFile path = fmap (loadLines . Text.lines) (readFile path)
 
 fileToGraph :: FilePath -> IO Graph
 fileToGraph path    = do
     dimacs <- loadFile path
-    let dimacsSize  = head [x | x@DimacsSize {} <- dimacs]
+    let dimacsSize  = fromMaybe undefined $ head [x | x@DimacsSize {} <- dimacs]
         dimacsEdges = [x | x@DimacsEdge {} <- dimacs]
         edges       = map (\(DimacsEdge x y) -> (x, y)) dimacsEdges
         bounds      = (1, nvertices dimacsSize)
