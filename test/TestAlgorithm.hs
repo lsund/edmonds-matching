8f2f04e9d388cfd1146f0b32723993efb98db8db
Ludvig Sundström <lud.sund@gmail.com>
2017-05-25 08:50:33 +0200

    improving TestAlgorithm

--- test/TestAlgorithm.hs
+++ test/TestAlgorithm.hs
@@ -2,50 +2,52 @@ module TestAlgorithm where
 
 import Protolude
 import Test.HUnit
+import Util
 
 import Edmond.Data.Graph as Graph
 import DIMACSParser
 import Edmond.Algorithm.Core
 
-test00 = TestCase   (do rep <- fileToGraph "data/P4.dmx"
-                        let init = Graph.initialize rep
-                            (_, graph) = findRoot init
-                        assertEqual
-                            ""
-                            [(1,3),(2,4)]
-                            (matching graph))
+----------------------------------------------------------------------------
+-- Data
 
-test01 = TestCase   (do rep <- fileToGraph "data/K2.dmx"
-                        let init = Graph.initialize rep
-                            (_, graph) = findRoot init
-                        assertEqual
-                            ""
-                            1
-                            (length $ matching graph))
+paths = [ "data/P4.dmx"
+        , "data/K2.dmx"
+        , "data/K4.dmx"
+        , "data/bipartite-simple.dmx"
+        , "data/bipartite.dmx"
+        ]
 
-test02 = TestCase   (do rep <- fileToGraph "data/K4.dmx"
-                        let init = Graph.initialize rep
-                            (_, graph) = findRoot init
-                        assertEqual
-                            ""
-                            2
-                            (length $ matching graph))
+expectedLengths = [ 2
+                  , 1
+                  , 2
+                  , 2
+                  , 4
+                  ]
 
-test03 = TestCase   (do rep <- fileToGraph "data/bipartite-simple.dmx"
-                        let init = Graph.initialize rep
-                            (_, graph) = findRoot init
-                        assertEqual
-                            ""
-                            2
-                            (length $ matching graph))
+----------------------------------------------------------------------------
+-- Utils
 
-test04 = TestCase   (do rep <- fileToGraph "data/bipartite.dmx"
-                        let init = Graph.initialize rep
-                            (_, graph) = findRoot init
-                        assertEqual
-                            ""
-                            4
-                            (length $ matching graph))
+checkIfMatching graph = assertBool "" $ isMatching $ matching graph
 
-algoTests = TestList [test00, test01, test02, test03, test04]
+checkMatchingLen exp graph = assertEqual "" exp $ length $ matching graph
+
+testIfMatching path = TestCase  (do rep <- fileToGraph path
+                                    let init = Graph.initialize rep
+                                        (_, graph) = findRoot init
+                                    checkIfMatching graph)
+
+testMatchingLen (path, len) = TestCase  (do rep <- fileToGraph path
+                                            let init = Graph.initialize rep
+                                                (_, graph) = findRoot init
+                                            checkMatchingLen len graph)
+
+----------------------------------------------------------------------------
+-- tests
+
+tests0 = map testIfMatching paths
+
+tests1 = zipWith (curry testMatchingLen) paths expectedLengths
+
+algoTests = TestList (tests0 ++ tests1)
 
